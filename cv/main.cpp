07506c8994df17cf63b31c04aaa30293e499e4ee
rodinf <rodinfm307@gmail.com>
2022-02-26 15:57:14 +0300

    upd

--- cv/main.cpp
+++ cv/main.cpp
@@ -1,22 +1,20 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
-
-using namespace std;
+#include "opencv2/opencv.hpp"
 using namespace cv;
 
-int main (int argc, char** argv) {
-    Mat
-        img_color = imread("images/test.png", 1),
-        img_grayscale = imread("images/test.png", 0);
-
-    imshow("color popov", img_color);
-    imshow("grayscale popov", img_grayscale);
-
-    waitKey(0);
-
-    destroyAllWindows();
-
-    imwrite("grayscale.jpg", img_grayscale);
-
+int main(int, char**) {
+    VideoCapture cap(0); // Открываем камеру или файл
+    if(!cap.isOpened())// check if we succeeded
+        return -1;
+    Mat edges;
+    namedWindow("edges",1);
+    while(1) {
+        Mat frame;
+        cap >> frame; // get a new frame from camera
+        cvtColor(frame, edges, COLOR_BGR2GRAY); // Перевод в градации серого
+        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5); // Размытие
+        Canny(edges, edges, 0, 30, 3); // Выделение границ
+        imshow("edges", edges);
+        if(waitKey(30) >= 0) break;
+    }
     return 0;
 }
